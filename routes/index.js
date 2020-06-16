d247152447beebb6a15db1d43170efaeef579b14
Anton Volkov <volkovanton71@yandex.ru>
2020-06-16 15:17:01 +0300

    correct eslint-bugs

--- routes/index.js
+++ routes/index.js
@@ -13,27 +13,28 @@ const cards = require('./cards');
 const notFound = require('./notFound');
 
 router.get('/crash-test', () => {
+  process.on('message', (msg) => {
+    if (msg === 'Сервер сейчас упадёт') {
+      // eslint-disable-next-line no-console
+      console.log('Closing all connections...');
+    }
+  });
+
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
 
-// process.on('message', function (msg) {
-//   if (msg == 'shutdown') {
-//     console.log('Closing all connections...');
-//   }
-// });
-
 router.use('/', badQuery);
 
 router.post('/signin', signInCelebrate, login);
-
 router.post('/signup', signUpCelebrate, postUser);
 
 router.use(auth);
 
 router.use('/users', users);
 router.use('/cards', cards);
+
 router.all('*', notFound);
 
 module.exports = router;
